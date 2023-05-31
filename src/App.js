efd8d24c588115efc3019ab85d8269666b531864
sophia.wings <sophia.wings@kakaocorp.com>
2023-05-31 10:00:18 +0900

    github page에서 빈화면으로 나타나는 이슈 해결, Route basename 추가

--- src/App.js
+++ src/App.js
@@ -10,17 +10,14 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">홈</Link>
-            </li>
-            <li>
-              <Link to="movie">영화 목록</Link>
+              <Link basename={process.env.PUBLIC_URL} to="/">홈</Link>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route basename={process.env.PUBLIC_URL} path="/" element={<Home />} />
           {/* id 자리 마련해둠. React Router한테 여기오는 id값이 무엇인지 알고싶다고 말함. 이 id대신에 다른 값을 써도됨. 이 url이 변수를 받을것이라 말해주는 것*/}
-          <Route path="/movie/:id" element={<Detail />} />
+          <Route basename={process.env.PUBLIC_URL} path="/movie/:id" element={<Detail />} />
         </Routes>
       </Router>
 
